5c05a471ae3da34800f27ff200b646fdc6218f05
golok727 <goooolok727@gmail.com>
2024-05-16 22:51:49 +0530

    fix: circular

--- packages/blocks/src/_common/components/index.ts
+++ packages/blocks/src/_common/components/index.ts
@@ -10,8 +10,4 @@ export * from './toast.js';
 export * from './tooltip/index.js';
 import './portal.js';
 
-export {
-  popTagSelect,
-  type SelectTag,
-} from '../../database-block/data-view/utils/tags/multi-tag-select.js';
 export { scrollbarStyle } from './utils.js';
